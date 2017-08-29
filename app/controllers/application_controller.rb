70a486b49c6cdffe7482a911ea47c434988787ce
Luis Lopez <luigibyte@gmail.com>
2017-08-28 23:49:08 -0300

    add scss bootstrap files to improve customizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || profile_path
   end
+
+  def after_sign_out_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || new_user_session_path
+  end
 end
