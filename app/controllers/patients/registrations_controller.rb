124c92a1ffabbb21286c41988ab769b601183372
Luis Lopez <luigibyte@gmail.com>
2017-09-11 23:19:31 -0300

    wip

--- app/controllers/patients/registrations_controller.rb
+++ app/controllers/patients/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Patients::RegistrationsController < Devise::RegistrationsController
+  layout 'box', only: [:new, :create]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
